7f54da39c50d79331785e727a37065b25c1cdc30
Godswill Udida <udidagodswill7@gmail.com>
2024-04-05 04:15:55 +0100

    node

--- node.ts
+++ node.ts
@@ -1,6 +1,13 @@
 import * as assert from 'assert';
 // import fs from "fs"
-import {Buffer} from "buffer"
+import { Buffer } from "buffer"
+import { IncomingMessage, ServerResponse } from 'http';
+
+import https from "https"
+
+import OS from "os";
+
+
 
 // const content = "Welcome to fs in node"
 
@@ -66,11 +73,21 @@ import {Buffer} from "buffer"
 // Creating a buffer of 10 Octets
 
 // const createBuffer = Buffer.alloc(10);
-const createBuffer = Buffer.allocUnsafe(10);
 
-console.log(createBuffer);
+// const createBuffer = Buffer.allocUnsafe(10);
+
+// creates a buffer based on the second parameter
 
 
+// const createBuffer = Buffer.alloc(36, "My name is Udida Godswill");
+
+
+// console.log(createBuffer);
+
+// converts a buffer to a string or plain text
+
+// const text = createBuffer.toString();
+// console.log(text)
 
 
 // const a: string = "GeeksforGeeks";
@@ -82,3 +99,75 @@ console.log(createBuffer);
 // } catch (error:any) {
 //     console.error(false);
 // }
+
+// let data = "i can and i will";
+
+// console.log(Buffer.from(data));
+
+// process.stdin.on("data", (message: any) => {
+//     console.log("Message", message);
+//     console.log(message.toString());
+// });
+   
+// const server: https.Server<typeof IncomingMessage, typeof ServerResponse> =
+//     https.createServer((req:IncomingMessage, res:ServerResponse) => {
+        
+//     });
+  
+// const port: number | any = 3000;
+// const port2: number | any = 3000;
+
+// server.listen(port, () => {
+//     console.log(`someone connected!`);
+//     // server.close(() => {
+//     //     process.exit(1);
+//     // })
+// })
+// server.listen(port2, () => {
+//     console.log(`someone connected!`);
+// })
+// server.on(port, (req: IncomingMessage, res: ServerResponse) => {
+//         console.log("Yes")
+// });
+
+
+
+// server.on("error", (e:Error | any) => {
+//   if (e === "EADDRINUSE") {
+//     console.error("Address in use, retrying...");
+//     setTimeout(() => {
+//       server.close();   
+//       server.listen(port);
+//     }, 1000);
+//   }
+// });
+
+const name = OS.hostname()
+
+// const getSystemPlatform = OS.platform()
+
+const a: string = OS.arch(); 
+const b: any = OS.userInfo(); 
+const c: string = OS.endianness(); 
+const d: number = OS.totalmem(); 
+const e: NodeJS.Dict<OS.NetworkInterfaceInfo[]> = OS.networkInterfaces(); 
+const f: number = OS.freemem(); 
+const g: number = OS.uptime(); 
+const h: string = OS.version(); 
+
+
+const i: any = OS.cpus();
+
+i.forEach((cpu :any, index :any) => {
+  console.log(`CPU ${index + 1}:`);
+  console.log(`Model: ${cpu.model}`);
+  console.log(`Speed: ${cpu.speed} MHz`);
+  console.log(`Times:`);
+  console.log(cpu.times);
+  console.log("\n");
+});
+
+console.log(); 
+
+
+// console.log(`The Name of Your Pc is ${name} and it have a total memory of ${d}  bytes and a free memory of ${f} bytes and it has been awake for ${g} seconds and it is running on ${h} version`)
